11fe43f9dcee9b70a086a28aaa3c966bea6e82a7
AxterDoesCode <chau.alex88@hotmail.com>
2025-07-24 01:25:59 +0000

    Adding some stuff

--- build.tcl
+++ build.tcl
@@ -1,3 +1,7 @@
+set_part xc7z020clg400-1
+set_property target_language Verilog [current_project]
+set_property board_part digilentinc.com:arty-z7-20:part0:1.1 [current_project]
+
 # read design sources (add one line for each file)
 read_verilog "verilog/mkTop.v"
 
