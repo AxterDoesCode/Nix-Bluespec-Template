d494994dccbc41f8915d449f9f17c5fa99708018
AxterDoesCode <chau.alex88@hotmail.com>
2025-08-01 11:41:59 +0100

    Add a clk_wiz into default template

--- arty.xdc
+++ arty.xdc
@@ -8,7 +8,7 @@ set_property -dict { PACKAGE_PIN H16    IOSTANDARD LVCMOS33 } [get_ports { CLK }
 create_clock -add -name sys_clk_pin -period 8.00 -waveform {0 4} [get_ports { CLK }];#set
 
 ## Switches
-set_property -dict { PACKAGE_PIN M20    IOSTANDARD LVCMOS33 } [get_ports { RST_N }]; #IO_L7N_T1_AD2N_35 Sch=SW0
+# set_property -dict { PACKAGE_PIN M20    IOSTANDARD LVCMOS33 } [get_ports { RST_N }]; #IO_L7N_T1_AD2N_35 Sch=SW0
 #set_property -dict { PACKAGE_PIN M19    IOSTANDARD LVCMOS33 } [get_ports { sw[1] }]; #IO_L7P_T1_AD2P_35 Sch=SW1
 
 ## RGB LEDs
--- bd/create_bd.tcl
+++ bd/create_bd.tcl
@@ -26,14 +26,32 @@ set reset_system [
     create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 proc_sys_reset
 ]
 
+set clock_generator [
+    create_bd_cell -type ip -vlnv xilinx.com:ip:clk_wiz:6.0 clk_wiz
+]
+
+set_property -dict [list \
+    CONFIG.PRIM_SOURCE {Single_ended_clock_capable_pin} \
+    CONFIG.PRIM_IN_FREQ {125.000} \
+    CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {25.000} \
+    CONFIG.USE_LOCKED {true} \
+    CONFIG.LOCKED_PORT {locked} \
+    CONFIG.USE_RESET {true} \
+    CONFIG.RESET_TYPE {ACTIVE_LOW} \
+    CONFIG.RESET_PORT {resetn}
+] $clock_generator
+
 create_bd_port -dir O led
 
 
 connect_bd_net [get_bd_pins proc_sys_reset/slowest_sync_clk] [get_bd_pins processing_system7/FCLK_CLK0]
 connect_bd_net [get_bd_pins proc_sys_reset/ext_reset_in] [get_bd_pins processing_system7/FCLK_RESET0_N]
 
-connect_bd_net [get_bd_pins processing_system7/FCLK_CLK0] [get_bd_pins top_0/CLK]
+connect_bd_net [get_bd_pins processing_system7/FCLK_CLK0] [get_bd_pins clk_wiz/clk_in1]
+connect_bd_net [get_bd_pins clk_wiz/clk_out1] [get_bd_pins top_0/CLK]
+
 connect_bd_net [get_bd_pins proc_sys_reset/peripheral_aresetn] [get_bd_pins top_0/RST_N]
+connect_bd_net [get_bd_pins proc_sys_reset/peripheral_aresetn] [get_bd_pins clk_wiz/resetn]
 connect_bd_net [get_bd_ports led] [get_bd_pins top_0/led]
 
 regenerate_bd_layout
